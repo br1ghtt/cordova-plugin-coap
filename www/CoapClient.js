0130890f0623b63bad56366c52cd4283f6fb46fb
Cedric Buehler <cedricbue@gmail.com>
2018-08-26 12:50:50 +0200

    CoapClient as js object

---
+++ www/CoapClient.js
@@ -0,0 +1,16 @@
+var exec = require('cordova/exec');
+
+function CoapClient() {
+
+}
+CoapClient.prototype.get = function(name, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "Coap", "get", [name]);
+};
+CoapClient.prototype.post = function(name, options, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "Coap", "post", [name, options]);
+};
+CoapClient.prototype.test = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "Coap", "test");
+};
+
+modules.export = new CoapClient();
